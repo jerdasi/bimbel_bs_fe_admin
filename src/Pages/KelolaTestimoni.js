8a0578bc567d0120e8f2e8b2c14800701753260a
jerdasi <jeremiadansil@gmail.com>
2022-06-28 16:53:03 +0700

    Slicing Page Testimoni

--- src/Pages/KelolaTestimoni.js
+++ src/Pages/KelolaTestimoni.js
@@ -1,12 +1,13 @@
 import React from "react";
 import SideBar from "../Components/SideBar";
+import TabelTestimoni from "../Components/TabelTestimoni";
 import TableSiswa from "../Components/TableSiswa";
 
 export default function KelolaTestimoni() {
     return (
         <div className="w-full md:w-screen md:h-screen md:flex p-2 pb-16 md:p-0">
             <SideBar />
-            <div>Belum Siap Halamannya</div>
+            <TabelTestimoni />
         </div>
     );
 }
